e98d1411175261b8cf8ad92d64fc8ed5f6404e2c
saumya2017 <saumya.srivastava@innovaccer.com>
2017-01-24 16:35:51 +0530

    Add files via upload

--- Twitter1.py
+++ Twitter1.py
@@ -7,14 +7,13 @@ from textblob import TextBlob
 #Set parameters
 f = open("input.txt", "r")
 keyword = (f.read().split("\n"))[0]
-tweetsXiteration = 100
-done = False; #to collect more data
+# keyword = ""
 
 #Setting the OAuth
-Consumer_Key = '3gN0mh60RKpUnyvPY3LOgOH2M';
-Consumer_Secret = 'QwcetTY1e4dbah4iTzn3pxqPUvZ3WJdrDdHRO5TmfstGMa3JV2';
-Access_Token = '358637575-rjNVSGkO8DeVompLgZS7sSJ9VDkArPdfAhXwhWz1';
-Access_Token_Secret = 'TbUQkO0kHlYrXh9KgcPEvuYjtviBxKbJnUIcC82CyzIJE';
+Consumer_Key      = "BKfEKIzuSSqgeRXOV85Wi2njY"
+Consumer_Secret   = "tNuMramPgpToooXiN5ZwJtycIDvDvLgZHiXeyKozNeLBse773A"
+Access_Token       = "767396202358923264-NqYRuLNhp3cDiKC7AyL6kQwc08rsMTR"
+Access_Token_Secret = "QC89GzQzbTVlLjqfqfx7faQFnDCyAYaP0TfXJYjq06u6C"
 
 #Connection established with Twitter API v1.1
 twitter = Twython(Consumer_Key, Consumer_Secret, Access_Token, Access_Token_Secret);
@@ -23,69 +22,93 @@ twitter = Twython(Consumer_Key, Consumer_Secret, Access_Token, Access_Token_Secr
 #dictionary wo
 dictionary = {}
 dict1 = {}
-if keyword == 'bag':
-    myList = ['Chanel', 'Fendi', 'Hermes', 'Louis Vuitton', 'Marc Jacobs', 'Prada', 'Kate spade', 'coach']
-elif keyword == 'belt':
-    myList = ['nautica', 'louis vuitton', 'woodland', 'fossil', 'hermes', 'armani', 'gucci', 'calvin klein', 'tommy hilfiger']
-elif keyword == 'lipstick':
-    myList = ['revlon', 'lakme', 'loreal', 'colorbar', 'mac', 'nyx', 'maybelline']
-elif keyword == 'sunglass':
-    myList = ['Dolce & Gabbana', 'Burberry', 'Versace', 'Emporio Armani', 'Prada', 'Gucci', 'Fendi', 'Maui Jim', 'Ray Ban', 'Oakley']
 
+metadata = [
+    {
+        'product': 'bags',
+        'values': ['Chanel', 'Fendi', 'Hermes', 'Louis Vuitton', 'Marc Jacobs', 'Prada', 'Kate spade', 'coach']
+    },
+    {
+        'product': 'belts',
+        'values': ['nautica', 'louis vuitton', 'woodland', 'fossil', 'hermes', 'armani', 'gucci', 'calvin klein', 'tommy hilfiger']
+    },
+    {
+        'product': 'lipsticks',
+        'values': ['revlon', 'lakme', 'loreal', 'colorbar', 'mac', 'nyx', 'maybelline']
+    },
+    {
+        'product': 'sunglasses',
+        'values': ['Dolce & Gabbana', 'Burberry', 'Versace', 'Emporio Armani', 'Prada', 'Gucci', 'Fendi', 'Maui Jim', 'Ray Ban', 'Oakley']
+    }
+]
 
 client = MongoClient()
 db = client.tweet_db
 tweet_collection = db.tweet_collection
 output_collection = db.output_collection
+#
+# def createMetadata():
+#     for info in metadata:
+#         db.metadata.save(info)
+#
+# createMetadata()
 
+def createData(keyword):
+    myList = []
+    for obj in metadata:
+        if keyword == obj['product']:
+            myList = obj['values']
 
-for i in myList:
-#Twitter is queried
-    countTweets = 0
-    polarity = 0
-    response = twitter.search(q = i, count = tweetsXiteration, result_type = 'mixed');
-    tweet_collection.insert(response["statuses"])
-    #Results (partial)
-    countTweets = len(response['statuses']);
-    # print countTweets
+    tweetsXiteration = 100
+    done = False; #to collect more data
+    for i in myList:
+        # print i
+        #Twitter is queried
+        countTweets = 0
+        polarity = 0
+        response = twitter.search(q = i, count = tweetsXiteration, result_type = 'mixed');
+        tweet_collection.insert(response["statuses"])
+        #Results (partial)
+        countTweets = len(response['statuses']);
+        # print countTweets
 
-    #If all the tweets have been fetched, then we are done
-    if not ('next_results' in response['search_metadata']):
-        done = True;
-
-    #If not all the tweets have been fetched, then...
-    while (done == False):
-
-    #Parsing information for maxID
-        parse1 = response['search_metadata']['next_results'].split("&");
-        parse2 = parse1[0].split("?max_id=");
-        parse3 = parse2[1];
-        maxID = parse3;
-
-    #Twitter is queried (again, this time with the addition of 'max_id')
-        response = twitter.search(q = i, count = tweetsXiteration, max_id = maxID, include_entities = 1, result_type = 'mixed');
-        # print response
-        for statuse in response["statuses"]:
-            tweet_collection.insert(statuse)
-        tweet_cursor = tweet_collection.find({})
-        for document in tweet_cursor:
-            # print document
-            # text = document["text"]
-            # blob = TextBlob(text)
-            blob = TextBlob(document["text"])
-            polarity = polarity + blob.sentiment.polarity
-            # document['polarity'] = polarity
-            # print document['polarity']
-    #Updating the total amount of tweets fetched
-        countTweets = countTweets + len(response['statuses']);
-        polarity = polarity / len(response['statuses'])
-    #If all the tweets have been fetched, then we are done
+        #If all the tweets have been fetched, then we are done
         if not ('next_results' in response['search_metadata']):
             done = True;
 
-    #print(countTweets);
-    dictionary[i] = countTweets
-    # dict1[i] = polarity
-print dictionary
-# print dict1
-output_collection.insert({"title":"myList","output":dictionary})
+        #If not all the tweets have been fetched, then...
+        while (done == False):
+        #Parsing information for maxID
+            parse1 = response['search_metadata']['next_results'].split("&");
+            parse2 = parse1[0].split("?max_id=");
+            parse3 = parse2[1];
+            maxID = parse3;
+
+            #Twitter is queried (again, this time with the addition of 'max_id')
+            response = twitter.search(q = i, count = tweetsXiteration, max_id = maxID, include_entities = 1, result_type = 'mixed');
+            # print response
+            for statuse in response["statuses"]:
+                tweet_collection.insert(statuse)
+            tweet_cursor = tweet_collection.find({})
+            for document in tweet_cursor:
+                # print document
+                # text = document["text"]
+                # blob = TextBlob(text)
+                blob = TextBlob(document["text"])
+                polarity = polarity + blob.sentiment.polarity
+                # document['polarity'] = polarity
+                # print document['polarity']
+                #Updating the total amount of tweets fetched
+            countTweets = countTweets + len(response['statuses']);
+            polarity = polarity / len(response['statuses'])
+            #If all the tweets have been fetched, then we are done
+            if not ('next_results' in response['search_metadata']):
+                done = True;
+
+        #print(countTweets);
+        dictionary[i] = countTweets
+        # dict1[i] = polarity
+    # print dictionary
+    # print dict1
+    output_collection.insert({"title":keyword,"output":dictionary})
+    return dictionary
--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
-import Twitter1
+from flask import Flask, render_template, request, jsonify
+import Twitter1 as analyseTweets
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route('/result')
-def result():
-    return render_template('result.html', result = Twitter1.dictionary)
+@app.route('/product/<product_name>')
+def renderProductPage(product_name):
+    return render_template('result.html', result= analyseTweets.createData(product_name))
+
+@app.route('/topBrands', methods=['GET'])
+def getTopBrandsInformation():
+    product_name = request.args.get('product')
+    data = analyseTweets.createData(product_name)
+    return jsonify(**data)
 
 if __name__ == '__main__':
     app.run(debug = True)
